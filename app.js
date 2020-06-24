6db421fb8fc0e6bf20dd3fc19da500a5e49069f1
hamet123 <ayush_94@live.com>
2020-06-24 06:35:18 +0000

    Commit on date 24/06/2020

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.use(indexRoutes);
 // 	Listening to the Port
 // =============================
 
-app.listen(3000,()=> {
+app.listen(4000,()=> {
 	console.log("Server started successfully");
 });
